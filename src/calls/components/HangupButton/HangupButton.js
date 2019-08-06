0671e72846c2cbf56f70280a937153a4305ee030
Rene Fernandez Sanchez <rene.fernandez@cern.ch>
2019-08-06 13:58:15 +0200

    Use hooks on HangupButton

--- src/calls/components/HangupButton/HangupButton.js
+++ src/calls/components/HangupButton/HangupButton.js
@@ -1,60 +1,34 @@
-import { StyleSheet, View, TouchableOpacity } from 'react-native';
+import { View, TouchableOpacity } from 'react-native';
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Icon } from 'react-native-elements';
 
 import { withPhoneService } from '../../providers/PhoneProvider/PhoneService';
+import styles from './HangupButtonStyles';
 
-const styles = StyleSheet.create({
-  button: {
-    marginTop: 10,
-    marginBottom: 10
-  },
-  hangupButtonContainer: {
-    alignItems: 'center'
-  },
-  hangupButton: {
-    alignItems: 'center',
-    justifyContent: 'center',
-    width: 70,
-    height: 70,
-    backgroundColor: '#f12121',
-    borderRadius: 50,
-    transform: [{ rotate: '135deg' }]
-  }
-});
-
-class HangupForm extends React.Component {
-  static propTypes = {
-    phoneService: PropTypes.shape({
-      hangUpCurrentCallAction: PropTypes.func.isRequired
-    }).isRequired
-  };
-
-  hangupCall = () => {
-    const { phoneService } = this.props;
-    console.log(`Hanging up call`);
+function HangupForm({ phoneService }) {
+  const hangupCall = () => {
     phoneService.hangUpCurrentCallAction();
   };
 
-  /**
-   * Render the component
-   * @returns {*}
-   */
-  render() {
-    return (
-      <View style={styles.hangupButtonContainer}>
-        <TouchableOpacity style={styles.hangupButton} onPress={this.hangupCall}>
-          <Icon
-            name="call"
-            size={25}
-            color="white"
-            style={{ transform: [{ rotate: '90deg' }] }}
-          />
-        </TouchableOpacity>
-      </View>
-    );
-  }
+  return (
+    <View style={styles.hangupButtonContainer}>
+      <TouchableOpacity style={styles.hangupButton} onPress={hangupCall}>
+        <Icon
+          name="call"
+          size={25}
+          color="white"
+          style={{ transform: [{ rotate: '90deg' }] }}
+        />
+      </TouchableOpacity>
+    </View>
+  );
 }
+
+HangupForm.propTypes = {
+  phoneService: PropTypes.shape({
+    hangUpCurrentCallAction: PropTypes.func.isRequired
+  }).isRequired
+};
 
 export default withPhoneService(HangupForm);
