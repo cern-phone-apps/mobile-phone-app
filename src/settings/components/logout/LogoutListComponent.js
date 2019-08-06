14febffe4726363faeb9fe72f7ae3001a461d5f8
Rene Fernandez Sanchez <rene.fernandez@cern.ch>
2019-08-06 15:30:33 +0200

    Add bottom divider to LogoutListComponent

--- src/settings/components/logout/LogoutListComponent.js
+++ src/settings/components/logout/LogoutListComponent.js
@@ -29,9 +29,9 @@ const LogoutListComponent = ({ logout, navigation, phoneService }) => {
     <ListItem
       key="logout"
       title="Log out"
-      topDivider
       leftIcon={{ name: 'logout', type: 'material-community' }}
       onPress={() => logoutFunc(logout, navigation, phoneService)}
+      bottomDivider
     />
   );
 };
