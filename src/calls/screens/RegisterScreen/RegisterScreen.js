e67801182ceaa5a9b3d2c37a6af294a5574ef829
Rene Fernandez Sanchez <rene.fernandez@cern.ch>
2019-08-06 15:31:20 +0200

    Use hooks on RegisterScreen

--- src/calls/screens/RegisterScreen/RegisterScreen.js
+++ src/calls/screens/RegisterScreen/RegisterScreen.js
@@ -1,141 +1,137 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { FlatList, View, Text, Linking } from 'react-native';
 import { Card, Button, Icon, Overlay } from 'react-native-elements';
 import RegisterForm from '../../components/RegisterForm/RegisterForm';
 
-export default class RegisterScreen extends React.Component {
-  static propTypes = {
-    connected: PropTypes.bool.isRequired,
-    numbers: PropTypes.arrayOf(PropTypes.object),
-    getUserPhoneNumbers: PropTypes.func.isRequired,
-    token: PropTypes.string
-  };
+export default function RegisterScreen({
+  getUserPhoneNumbers,
+  connected,
+  navigation,
+  numbers,
+  token
+}) {
+  const [overlayVisible, setOverlayVisible] = useState(false);
 
-  static defaultProps = {
-    token: '',
-    numbers: []
-  };
-
-  static navigationOptions = {
-    title: 'Select a phone number'
-  };
-
-  state = {
-    overlayVisible: false
-  };
-
-  componentDidMount = () => {
-    const { getUserPhoneNumbers } = this.props;
-    getUserPhoneNumbers();
-  };
-
-  componentDidUpdate = () => {
-    const { connected, navigation } = this.props;
-
+  useEffect(() => {
     if (connected) {
       navigation.navigate('AppRegistered');
+    } else {
+      getUserPhoneNumbers();
     }
-  };
+  }, [connected]);
 
-  keyExtractor = (item, index) => index.toString();
+  const keyExtractor = (item, index) => index.toString();
 
-  renderItem = ({ item }) => {
-    const { token } = this.props;
+  const renderItem = ({ item }) => {
     return <RegisterForm phoneNumber={item.phoneNumber} token={token} />;
   };
 
-  render = () => {
-    const { numbers } = this.props;
+  renderItem.propTypes = {
+    item: PropTypes.shape({
+      phoneNumber: PropTypes.string.isRequired
+    }).isRequired
+  };
 
-    return (
-      <View
-        style={{
-          flex: 1,
-          backgroundColor: 'white'
-        }}
+  return (
+    <View
+      style={{
+        flex: 1,
+        backgroundColor: 'white'
+      }}
+    >
+      <Overlay
+        isVisible={overlayVisible}
+        onBackdropPress={() => setOverlayVisible(false)}
       >
-        <Overlay
-          isVisible={this.state.overlayVisible}
-          onBackdropPress={() => this.setState({ overlayVisible: false })}
+        <View
+          style={{
+            flex: 1,
+            flexDirection: 'column',
+            justifyContent: 'center',
+            alignItems: 'center'
+          }}
         >
-          <View
+          <Text style={{ fontSize: 34 }}>About Certificates</Text>
+          <Text
             style={{
-              flex: 1,
-              flexDirection: 'column',
-              justifyContent: 'center',
-              alignItems: 'center'
+              textAlign: 'justify'
             }}
           >
-            <Text style={{ fontSize: 34 }}>About Certificates</Text>
-            <Text
+            This application requires CERN Certificates to work. Please, follow
+            the steps to install them from the following link and come back to
+            the application. You may need to restart the application afterwards.
+          </Text>
+          <Button
+            title="https://cafiles.cern.ch/cafiles/certificates/Grid.aspx"
+            type="clear"
+            onPress={() => {
+              Linking.openURL(
+                'https://cafiles.cern.ch/cafiles/certificates/Grid.aspx'
+              );
+            }}
+          />
+          <Button title="Close" onPress={() => setOverlayVisible(false)} />
+        </View>
+      </Overlay>
+
+      <FlatList
+        keyExtractor={keyExtractor}
+        data={numbers}
+        renderItem={renderItem}
+      />
+      <View style={{ flex: 1, backgroundColor: '000' }}>
+        <Card
+          title={
+            <View
               style={{
-                textAlign: 'justify'
+                flexDirection: 'row',
+                justifyContent: 'center',
+                alignItems: 'center',
+                borderBottomColor: '#d4d4d4',
+                borderBottomWidth: 1
               }}
             >
-              This application requires CERN Certificates to work. Please,
-              follow the steps to install them from the following link and come
-              back to the application. You may need to restart the application
-              afterwards.
-            </Text>
-            <Button
-              title="https://cafiles.cern.ch/cafiles/certificates/Grid.aspx"
-              type="clear"
-              onPress={() => {
-                Linking.openURL(
-                  'https://cafiles.cern.ch/cafiles/certificates/Grid.aspx'
-                );
-              }}
-            />
-            <Button
-              title="Close"
-              onPress={() => this.setState({ overlayVisible: false })}
-            />
-          </View>
-        </Overlay>
-
-        <FlatList
-          keyExtractor={this.keyExtractor}
-          data={numbers}
-          renderItem={this.renderItem}
-        />
-        <View style={{ flex: 1, backgroundColor: '000' }}>
-          <Card
-            title={
-              <View
+              <Icon name="info-circle" type="font-awesome" />
+              <Text
                 style={{
-                  flexDirection: 'row',
-                  justifyContent: 'center',
-                  alignItems: 'center',
-                  borderBottomColor: '#d4d4d4',
-                  borderBottomWidth: 1
+                  fontSize: 16,
+                  fontWeight: 'bold'
                 }}
               >
-                <Icon name="info-circle" type="font-awesome" />
-                <Text
-                  style={{
-                    fontSize: 16,
-                    fontWeight: 'bold'
-                  }}
-                >
-                  {' '}
-                  About Certificates
-                </Text>
-              </View>
-            }
-          >
-            <Text style={{ marginTop: 5 }}>
-              Please, install the CERN certificates and come back to this screen
-              if you haven&apos;t done it yet.
-            </Text>
-            <Button
-              title="More info"
-              onPress={() => this.setState({ overlayVisible: true })}
-              type="clear"
-            />
-          </Card>
-        </View>
+                {' '}
+                About Certificates
+              </Text>
+            </View>
+          }
+        >
+          <Text style={{ marginTop: 5 }}>
+            Please, install the CERN certificates and come back to this screen
+            if you haven&apos;t done it yet.
+          </Text>
+          <Button
+            title="More info"
+            onPress={() => setOverlayVisible(true)}
+            type="clear"
+          />
+        </Card>
       </View>
-    );
-  };
+    </View>
+  );
 }
+
+RegisterScreen.propTypes = {
+  connected: PropTypes.bool.isRequired,
+  numbers: PropTypes.arrayOf(PropTypes.object),
+  getUserPhoneNumbers: PropTypes.func.isRequired,
+  token: PropTypes.string
+};
+
+RegisterScreen.defaultProps = {
+  token: '',
+  numbers: []
+};
+
+RegisterScreen.navigationOptions = {
+  title: 'Select a phone number'
+};
