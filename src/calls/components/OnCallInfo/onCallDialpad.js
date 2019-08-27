51f0be96b32561bba622535f392d2bf669ba18b6
lopez <llopez@student.42.fr>
2019-08-27 10:29:36 +0200

    prettiered

--- src/calls/components/OnCallInfo/onCallDialpad.js
+++ src/calls/components/OnCallInfo/onCallDialpad.js
@@ -1,64 +1,78 @@
 import { View, StyleSheet, TouchableOpacity } from 'react-native';
 import React from 'react';
-import Dialpad from '../DialpadForm/Dialpad/Dialpad';
 import { Icon } from 'react-native-elements';
+import Dialpad from '../DialpadForm/Dialpad/Dialpad';
 import ColorPalette from '../../../styles/ColorPalette';
 
-
 const dialpadstyles = StyleSheet.create({
-    button: {
-        marginTop: 10,
-        marginBottom: 10
-    },
-    DialpadButtonContainer: {
-        alignItems: 'center',
-        top: "35%"
-    },
-    DialpadButton: {
-        alignItems: 'center',
-        justifyContent: 'center',
-        width: 70,
-        height: 70,
-        backgroundColor: ColorPalette.primary,
-        borderRadius: 50
-    },
-    CloseDialpadButton: {
-        alignItems: 'center',
-        justifyContent: 'center',
-        width: 40,
-        height: 40,
-        backgroundColor: ColorPalette.primary,
-        borderRadius: 50
-    }
-  });
+  button: {
+    marginTop: 10,
+    marginBottom: 10
+  },
+  DialpadButtonContainer: {
+    alignItems: 'center',
+    top: '35%'
+  },
+  DialpadButton: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: 70,
+    height: 70,
+    backgroundColor: ColorPalette.primary,
+    borderRadius: 50
+  },
+  CloseDialpadButton: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: 40,
+    height: 40,
+    backgroundColor: ColorPalette.primary,
+    borderRadius: 50
+  }
+});
 
 function DialpadButton({ display, onPress, updatePhoneNumber }) {
-  if (display) return <View style={{ width: "100%", top: dialpadstyles.DialpadButtonContainer.top, flex: 1 }}>
-      <View style={{
-        alignItems: 'center',
-        width: '100%',
-        marginBottom: 20
-      }}>
-        <TouchableOpacity style={dialpadstyles.CloseDialpadButton} onPress={() => { onPress(); }}>
-          <Icon
-            name="close"
-            size={15}
-            color="white"
-            type="ionicons"
-          />
-        </TouchableOpacity>
-      </View>            
-            <Dialpad updatePhoneNumber={(e) => { updatePhoneNumber(e) }}/>
-                      </View>;
+  if (display)
+    return (
+      <View
+        style={{
+          width: '100%',
+          top: dialpadstyles.DialpadButtonContainer.top,
+          flex: 1
+        }}
+      >
+        <View
+          style={{
+            alignItems: 'center',
+            width: '100%',
+            marginBottom: 20
+          }}
+        >
+          <TouchableOpacity
+            style={dialpadstyles.CloseDialpadButton}
+            onPress={() => {
+              onPress();
+            }}
+          >
+            <Icon name="close" size={15} color="white" type="ionicons" />
+          </TouchableOpacity>
+        </View>
+        <Dialpad
+          updatePhoneNumber={e => {
+            updatePhoneNumber(e);
+          }}
+        />
+      </View>
+    );
   return (
     <View style={dialpadstyles.DialpadButtonContainer}>
-      <TouchableOpacity style={dialpadstyles.DialpadButton} onPress={() => { onPress(); }}>
-        <Icon
-          name="dialpad"
-          size={25}
-          color="white"
-          type="ionicons"
-        />
+      <TouchableOpacity
+        style={dialpadstyles.DialpadButton}
+        onPress={() => {
+          onPress();
+        }}
+      >
+        <Icon name="dialpad" size={25} color="white" type="ionicons" />
       </TouchableOpacity>
     </View>
   );
