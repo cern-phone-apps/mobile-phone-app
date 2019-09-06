ccaea533bb0143ab1df310559c42865a8b0a906f
Rene Fernandez Sanchez <rene.fernandez@cern.ch>
2019-09-06 15:35:27 +0200

    Implement background messaging with access to store

--- bgMessaging.js
+++ bgMessaging.js
@@ -1,10 +1,13 @@
 import firebase from 'react-native-firebase';
-import { RemoteMessage } from 'react-native-firebase';
+import {RemoteMessage} from 'react-native-firebase';
 
-export default async message => {
+export default async (store, message) => {
   // handle your message
   console.log('Receiving a background PUSH Notification');
-  console.log(JSON.stringify(message));
+  console.log(message);
+  store.subscribe(() => {
+    console.log(store.getState().auth);
+  });
 
   return Promise.resolve();
 };
