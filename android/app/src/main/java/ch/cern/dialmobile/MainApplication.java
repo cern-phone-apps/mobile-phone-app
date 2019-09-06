cd3845e89fdc6264aa6cb9f52551cd766e34dc0e
Rene Fernandez Sanchez <rene.fernandez@cern.ch>
2019-09-06 15:34:55 +0200

    Update to react native 0.60.5

--- android/app/src/main/java/ch/cern/dialmobile/MainApplication.java
+++ android/app/src/main/java/ch/cern/dialmobile/MainApplication.java
@@ -1,8 +1,11 @@
 package ch.cern.dialmobile;
 
 import android.app.Application;
+import android.util.Log;
 
 import com.facebook.react.PackageList;
+import com.facebook.hermes.reactexecutor.HermesExecutorFactory;
+import com.facebook.react.bridge.JavaScriptExecutorFactory;
 import com.facebook.react.ReactApplication;
 import com.oney.WebRTCModule.WebRTCModulePackage;
 import io.wazo.callkeep.RNCallKeepPackage;
