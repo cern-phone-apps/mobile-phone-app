6f6369e90c4e4e687fc4130da160bcb3d9753103
Rene Fernandez Sanchez <rene.fernandez@cern.ch>
2019-09-10 09:55:23 +0200

    Update eslintrc and eslintignore

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,11 @@ module.exports = {
   root: true,
   extends: ['@react-native-community', 'airbnb'],
   rules: {
-    "object-curly-newline": 0,
-    "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }]
-  }
+    'object-curly-newline': 0,
+    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
+    'no-console': 0,
+    'space-before-function-paren': ['error', 'always'],
+    'react/jsx-closing-bracket-location': 0,
+    'comma-dangle': ['error', 'always'],
+  },
 };
