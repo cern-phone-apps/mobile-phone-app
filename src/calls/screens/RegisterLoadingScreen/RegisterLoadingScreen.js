f9565ca8976a30b000e3b4e96f2c8c3a393949d1
Rene Fernandez Sanchez <rene.fernandez@cern.ch>
2019-11-08 14:48:06 +0100

    Remove comment

--- src/calls/screens/RegisterLoadingScreen/RegisterLoadingScreen.js
+++ src/calls/screens/RegisterLoadingScreen/RegisterLoadingScreen.js
@@ -6,7 +6,6 @@ import { logMessage } from '../../../common/utils/logging';
 const RegisterLoadingScreen = ({ connected, calling, onCall, navigation }) => {
   // Fetch the token from storage then navigate to our appropriate place
   const handleRegistration = () => {
-    // const userToken = await AsyncStorage.getItem("userToken");
     logMessage(`RegisterLoadingScreen ->  connected? ${connected}`);
     // This will switch to the App screen or Auth screen and this loading
     // screen will be unmounted and thrown away.
