bc9cb40055cacda99fa8ecefb74e55d88333fa7e
Rene Fernandez Sanchez <rene.fernandez@cern.ch>
2019-11-08 14:48:31 +0100

    Use logMessage in configure-callkeep

--- configure-callkeep.js
+++ configure-callkeep.js
@@ -16,50 +16,53 @@ export default {
   }
 };
 
-/**
- * When displayIncomingCall is triggered, this is triggered (only on iOS?)
- */
-RNCallKeep.addEventListener('didDisplayIncomingCall', ({ error }) => {
-  // you might want to do following things when receiving this event:
-  // - Start playing ringback if it is an outgoing call
-  logMessage('RNCallkeep -> didDisplayIncomingCall');
-  if (error) {
-    errorMessage(error);
-  }
-});
+export const addRNCallKeepListeners = () => {
+  logMessage('configure-callkeep -> Adding callkeep listeners');
+  /**
+   * When displayIncomingCall is triggered, this is triggered (only on iOS?)
+   */
+  RNCallKeep.addEventListener('didDisplayIncomingCall', ({ error }) => {
+    // you might want to do following things when receiving this event:
+    // - Start playing ringback if it is an outgoing call
+    logMessage('RNCallkeep -> didDisplayIncomingCall');
+    if (error) {
+      errorMessage(error);
+    }
+  });
 
-/**
- * This is triggered when the user taps the NATIVE endCall button
- */
-RNCallKeep.addEventListener('endCall', ({ callUUID }) => {
-  logMessage('RNCallkeep -> endCall');
-  const { isInBackground } = store.getState().settings;
+  /**
+   * This is triggered when the user taps the NATIVE endCall button
+   */
+  RNCallKeep.addEventListener('endCall', ({ callUUID }) => {
+    logMessage('RNCallkeep -> endCall');
+    const { isInBackground } = store.getState().settings;
 
-  try {
-    toneAPI.hangUp();
-    logMessage(`RNCallkeep -> endCall ${callUUID}`);
-  } catch (error) {
-    errorMessage(error);
-  }
-  if (isInBackground) {
-    toneAPI.stopAgent();
-    RNCallKeep.setAvailable(false);
-  }
-});
+    try {
+      toneAPI.hangUp();
+      logMessage(`RNCallkeep -> endCall ${callUUID}`);
+    } catch (error) {
+      errorMessage(error);
+    }
+    if (isInBackground) {
+      toneAPI.stopAgent();
+      RNCallKeep.setAvailable(false);
+    }
+  });
 
-/**
- * This is triggered when the user taps the NATIVE answer button
- */
-RNCallKeep.addEventListener('answerCall', () => {
-  const { isInBackground } = store.getState().settings;
-  const { activeNumber } = store.getState().numbers;
+  /**
+   * This is triggered when the user taps the NATIVE answer button
+   */
+  RNCallKeep.addEventListener('answerCall', () => {
+    const { isInBackground } = store.getState().settings;
+    const { activeNumber } = store.getState().numbers;
 
-  logMessage('RNCallKeep -> AnswerCall from configureCallKeep');
-  logMessage(`RNCallKeep -> inBackground: ${isInBackground}`);
-  if (isInBackground) {
-    const password = store.getState().auth.toneToken;
-    toneAPI.authenticate(activeNumber, password, false);
-  } else {
-    toneAPI.answer();
-  }
-});
+    logMessage('RNCallKeep -> AnswerCall from configureCallKeep');
+    logMessage(`RNCallKeep -> inBackground: ${isInBackground}`);
+    if (isInBackground) {
+      const password = store.getState().auth.toneToken;
+      toneAPI.authenticate(activeNumber, password, false);
+    } else {
+      toneAPI.answer();
+    }
+  });
+};
