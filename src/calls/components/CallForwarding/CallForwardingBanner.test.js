440d9c4a48fa6e9ec7371446bbd4ea7246b793b7
Rene Fernandez Sanchez <rene.fernandez@cern.ch>
2019-11-21 16:25:38 +0100

    Update jest config and add first test test

--- src/calls/components/CallForwarding/CallForwardingBanner.test.js
+++ src/calls/components/CallForwarding/CallForwardingBanner.test.js
@@ -5,11 +5,18 @@
 
 import 'react-native';
 import React from 'react';
-import App from '../App';
-
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
 
+import CallForwardingBanner from './CallForwardingBanner';
+
 it('renders correctly', () => {
-  renderer.create(<App />);
+  const callForwardingConfig = {
+    callForwarding: {
+      'simultaneous-ring': false
+    }
+  };
+  renderer.create(
+    <CallForwardingBanner callForwarding={callForwardingConfig} />
+  );
 });
