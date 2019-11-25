5a3adfd9fe984215969daca135d2d29f26028c3d
Fiser <ruben.garcia.hernando@cern.ch>
2019-11-25 16:02:38 +0100

    Added Testing for SearchUsersScreen

---
+++ src/calls/screens/SearchUsersScreen/SearchUsersScreen.test.js
@@ -0,0 +1,137 @@
+import React from 'react';
+import {
+  waitForElement,
+  render,
+  fireEvent
+} from '@testing-library/react-native';
+import SearchUsersScreen from './SearchUsersScreen';
+
+test('Render the component', async () => {
+  const searchUsers = jest
+    .fn(() => {})
+    .mockReturnValueOnce(
+      Promise.resolve([
+        {
+          displayName: 'DisplayName1',
+          personId: 'PersonId1',
+          division: 'Division1'
+        }
+      ])
+    );
+  const addUserContact = jest.fn(() => {});
+
+  const getUserContacts = jest.fn(() => {});
+
+  const contacts = [
+    {
+      displayName: 'DisplayName1',
+      personId: 'PersonId1',
+      division: 'Division1'
+    },
+    {
+      displayName: 'DisplayName2',
+      personId: 'PersonId2',
+      division: 'Division2'
+    }
+  ];
+
+  const { baseElement, getByTestId } = render(
+    <SearchUsersScreen
+      addUserContact={addUserContact}
+      searchUsers={searchUsers}
+      getUserContacts={getUserContacts}
+      contacts={contacts}
+      searching={true}
+    />
+  );
+  expect(baseElement).toMatchSnapshot();
+  fireEvent.changeText(getByTestId('SearchUserScreenBar'), 'DisplayName');
+  fireEvent.press(getByTestId('SearchUserScreenButton'));
+  expect(baseElement).toMatchSnapshot();
+});
+
+test('Render the component with 2 caracters', async () => {
+  const searchUsers = jest
+    .fn(() => {})
+    .mockReturnValueOnce(
+      Promise.resolve([
+        {
+          displayName: 'DisplayName1',
+          personId: 'PersonId1',
+          division: 'Division1'
+        }
+      ])
+    );
+  const addUserContact = jest.fn(() => {});
+
+  const getUserContacts = jest.fn(() => {});
+
+  const contacts = [
+    {
+      displayName: 'DisplayName1',
+      personId: 'PersonId1',
+      division: 'Division1'
+    },
+    {
+      displayName: 'DisplayName2',
+      personId: 'PersonId2',
+      division: 'Division2'
+    }
+  ];
+
+  const { baseElement, getByTestId } = render(
+    <SearchUsersScreen
+      addUserContact={addUserContact}
+      searchUsers={searchUsers}
+      getUserContacts={getUserContacts}
+      contacts={contacts}
+      searching={true}
+    />
+  );
+  fireEvent.changeText(getByTestId('SearchUserScreenBar'), '12');
+  fireEvent.press(getByTestId('SearchUserScreenButton'));
+  expect(baseElement).toMatchSnapshot();
+});
+
+test('Render the component with empty', async () => {
+  const searchUsers = jest
+    .fn(() => {})
+    .mockReturnValueOnce(
+      Promise.resolve([
+        {
+          displayName: 'DisplayName1',
+          personId: 'PersonId1',
+          division: 'Division1'
+        }
+      ])
+    );
+  const addUserContact = jest.fn(() => {});
+
+  const getUserContacts = jest.fn(() => {});
+
+  const contacts = [
+    {
+      displayName: 'DisplayName1',
+      personId: 'PersonId1',
+      division: 'Division1'
+    },
+    {
+      displayName: 'DisplayName2',
+      personId: 'PersonId2',
+      division: 'Division2'
+    }
+  ];
+
+  const { baseElement, getByTestId } = render(
+    <SearchUsersScreen
+      addUserContact={addUserContact}
+      searchUsers={searchUsers}
+      getUserContacts={getUserContacts}
+      contacts={contacts}
+      searching={true}
+    />
+  );
+  fireEvent.changeText(getByTestId('SearchUserScreenBar'), '');
+  fireEvent.press(getByTestId('SearchUserScreenButton'));
+  expect(baseElement).toMatchSnapshot();
+});
